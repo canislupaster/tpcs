657ef27c9b9761a76b2fef8da08383d63a8e035a
supercilious <thomas@engineeringvirtue.net>
2022-09-22 20:41:09 -0700

    ???

---
+++ src/frag.glsl
@@ -0,0 +1,39 @@
+precision mediump float;
+
+varying mediump vec4 fdata;
+
+// https://github.com/hughsk/glsl-hsv2rgb/blob/master/index.glsl
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+void main() {
+  gl_FragColor = vec4(hsv2rgb(vec3(mix(0.86,0.98, noise(vec2(fdata.w,cos(fdata.y+fdata.x)))), fdata.w, noise(vec2(fdata.y,fdata.w)))), 1.0);
+}
---
+++ src/vert.glsl
@@ -0,0 +1,15 @@
+precision mediump float;
+
+attribute float spawnt;
+attribute float y;
+
+uniform float t;
+
+varying mediump vec4 fdata;
+
+void main(void) {
+  float pos = t-spawnt;
+  pos = pos<=0.0 ? 0.0 : (pos*pos + pos)/2.0;
+  gl_Position = vec4(2.0*pos-1.0,2.0*y-1.0,0,1);
+  fdata = vec4(spawnt,t,y,sin(t+spawnt));
+}
